46a9fa2a319142f8b0f30a02f85873481e7ad5c9
Tomas Villar <t.villar@alumno.um.edu.ar>
2025-05-01 22:19:32 -0300

    implemento las funciones Fibonacci recursiva y iterativa, tambien agrego la implementacion del programa

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,4 +1,35 @@
-def fibonacci_recursiva(n):
-    pass
-def fibonacci_iterativa(n):
-    pass
+def fibonacci_recursiva(n): #funcion fiboancci recursiva
+    if n < 0:
+        raise ValueError("Entrada inválida: el Fibonacci no acepta negativos")
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursiva(n-1) + fibonacci_recursiva(n-2)
+def fibonacci_iterativa(n): #funcion fiboancci iteraticva
+    if n < 0:
+        raise ValueError("Entrada inválida: el Fibonacci no acepta negativos")
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    a, b = 0, 1  # inicializamos los primeros dos valores de fibonacci
+    for _ in range(2, n + 1):
+        a, b = b, a + b  # actualizamos los valores iterativamente
+    return b
+
+def ejecucion_programa(): #implento la ejecución del programa
+    while True:
+        try:
+            numero = int(input("Ingrese un numero para calcular Fibonacci: "))
+            resultado1 = fibonacci_recursiva(numero)
+            print(f"el resultdo es {resultado1}")
+        except ValueError as e: # manejo de caso especial numero negativo
+           print(f"{e}")
+        except KeyboardInterrupt: # finalizar programa
+            print("\nPrograma finalizado.")
+            break
+if __name__ == "__main__":
+    ejecucion_programa()
